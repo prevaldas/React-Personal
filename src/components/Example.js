b7524d258728815d5c296a415ca1f70f8d4d3f93
Evaldas Pranckevicius <prevaldas@yahoo.com>
2022-07-05 08:48:20 +0100

    change SPA name in menu

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,24 +1,38 @@
-
 function Example() {
+  function disapier() {
+    document.querySelector('#menu-btn').addEventListener('click', function () {
+      document.querySelector('#menu-content').style.height = 'auto';
+      this.style.display = 'none';
+    });
+  }
 
-function disapier() {
-  document.querySelector('#menu-btn').addEventListener('click', function() {
-  document.querySelector('#menu-content').style.height= 'auto';
-  this.style.display= 'none';
-});
-    }
-
-    return <div className="four-container all">
-          <div className="dropdown-menu">
-          <button onMouseEnter={disapier} id="menu-btn">✓ More examples<i className="fa fa-caret-down"></i></button>
-          <div id="menu-content">
-                    <a className="links-hidden" href="https://warm-tanuki-218cc5.netlify.app"> ➤ Web page with React hooks and API.</a>
-          <a className="links-hidden" href="https://vandos-gelynas.netlify.app"> ➤ A personal web page with a hoby showcase.</a>
-          <a className="links-hidden" href="https://katinai.netlify.app"> ➤ Web page introducing pets and their activities.</a>
+  return (
+    <div className="four-container all">
+      <div className="dropdown-menu">
+        <button onMouseEnter={disapier} id="menu-btn">
+          ✓ More examples<i className="fa fa-caret-down"></i>
+        </button>
+        <div id="menu-content">
+          <a
+            className="links-hidden"
+            href="https://warm-tanuki-218cc5.netlify.app"
+          >
+            {' '}
+            ➤ SPA with React hooks and API.
+          </a>
+          <a className="links-hidden" href="https://vandos-gelynas.netlify.app">
+            {' '}
+            ➤ A personal web page with a hoby showcase.
+          </a>
+          <a className="links-hidden" href="https://katinai.netlify.app">
+            {' '}
+            ➤ Web page introducing pets and their activities.
+          </a>
         </div>
-        </div>
+      </div>
       <hr></hr>
     </div>
+  );
 }
 
 export default Example;
